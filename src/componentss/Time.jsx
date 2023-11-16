59d97011d4bb74191c5afc7dd8cf0d1a03aa4701
karan <ks888570@gmail.com>
2023-11-16 15:55:31 +0530

    useState, useEffect

--- src/componentss/Time.jsx
+++ src/componentss/Time.jsx
@@ -1,10 +1,24 @@
+import { useEffect, useState } from "react";
 
 function Time() {
-    let date = '26/10/2023';
-    let time = '10:38:17 AM'
+
+    const [ time, setTime ] = useState(new Date());
+
+    useEffect(() => {
+        console.log("interval has been setup")
+        const IntervalId = setInterval(() => {
+            setTime(new Date());
+        }, 1000)
+
+        return () => {
+            clearInterval(IntervalId);
+            console.log("cancelled the interval");
+        };
+    }, []);
+    // ek baar print ho isliye empty array pass kiye h
     return (
         <center className="Time fs-2">
-            This is the current time :{date} - {time}
+            This is the current time :{time.toLocaleDateString()} - {time.toLocaleTimeString()}
         </center>
     )
 }
